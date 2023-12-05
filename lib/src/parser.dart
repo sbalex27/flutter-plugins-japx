232143dc1a04cfdd1704ee5404d04ab5528f7998
Alex Escobedo <48226829+sbalex27@users.noreply.github.com>
2023-12-05 00:52:20 +0000

    Fix map check in parser.dart

--- lib/src/parser.dart
+++ lib/src/parser.dart
@@ -234,7 +234,7 @@ class Japx {
       }
       if (json[key] is Map<String, dynamic>) {
         final map = json[key] as Map<String, dynamic>?;
-        if (map == {}){
+        if (map.isEmpty){
           relationships[key] = {_data: null};
           json.remove(key);
           continue;
